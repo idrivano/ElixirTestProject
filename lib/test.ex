9e6d3900662e5d8b9443acd4f87fdda5244b3161
Adamou IDRISSOU <adamb2041@gmail.com>
2024-06-14 17:19:07 +0000

    Hello word test

--- lib/test.ex
+++ lib/test.ex
@@ -13,22 +13,9 @@ defmodule Test do
 
   """
   def hello do
+    IO.puts("Hello, World!")
     :world
   end
 end
 
-
-defmodule Projet do
-  def hello do
-    IO.puts "Bonjour, monde!"
-  end
-end
-
-defmodule ProjetTest do
-  use ExUnit.Case
-  doctest MonProjet
-
-  test "greets the world" do
-    assert capture_io(fn -> MonProjet.hello() end) == "Bonjour, monde!\n"
-  end
-end
+Test.hello()
